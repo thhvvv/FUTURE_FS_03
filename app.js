61f1556940fafddb24fa16111284854f048324b0
thhvvv <makausilvia964@gmail.com>
2024-11-19 09:18:46 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+let tasks = [];
+
+const addTask = ()=> {
+    const taskInput = document.getElementById('taskInput')
+    const text = taskInput.value
+}
+
+document.getElementById('newTask').addEventListener('clikc', function(e){
+    e.preventDefault()
+
+    addTask() 
+})
